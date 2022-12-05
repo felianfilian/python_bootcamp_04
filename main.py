c2435cdf49f64e7ecf86d4a0f2da6a4d4bd98fa0
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-05 13:20:51 +0100

    class methods

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ class User:
         print("object created")
         self.name = name
         self.id = id
+    def edit_id(self, id):
+        self.id = id
 
-
-    pass
 
 
 user_1 = User("Uschi", "901")
+
+user_1.edit_id("333")
 
 print(user_1.name + " - " + user_1.id)
 
