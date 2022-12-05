aace42e1a37a40fcdb7831c8e9683d4739f653a6
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-05 13:14:35 +0100

    first class test

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
+class User:
+    def __init__(self, name, id):
+        print("object created")
+        self.name = name
+        self.id = id
 
 
+    pass
+
+
+user_1 = User("Uschi", "901")
+
+print(user_1.name + " - " + user_1.id)
 
