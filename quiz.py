9d90b8d25e9644acec94b503202da7a08938aad0
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-06 08:52:19 +0100

    show question

--- quiz.py
+++ quiz.py
@@ -3,20 +3,16 @@ from quiz_data import question_data
 from quiz_brain import  QuizBrain
 
 def start():
+    lifes = 3
     question_bank = []
     for question in question_data:
         question_text = question["text"]
         question_answer = question["answer"]
         new_question = Question(question_text, question_answer)
         question_bank.append(new_question)
+    while lifes > 0:
+        print(f"LIFES: {lifes}")
+        quiz_brain = QuizBrain(question_bank)
+        if not quiz_brain.next_question():
+            lifes -= 1
 
-    quiz_brain = QuizBrain(question_bank)
-    choice = input(f"{quiz_brain.next_question().text} yea(a) or no(b)")
-    if choice == "a" and quiz_brain.next_question().answer == "True":
-        print("perfect")
-    else:
-        print("You are wrong")
-    if choice == "b" and quiz_brain.next_question().answer == "False":
-        print("perfect")
-    else:
-        print("You are wrong")
--- quiz_brain.py
+++ quiz_brain.py
@@ -5,5 +5,11 @@ class QuizBrain:
         self.question_bank = question_bank
 
     def next_question(self):
-        return self.question_bank[self.question_number]
+        current_question = self.question_bank[self.question_number]
+        choice = input(f"Q.{self.question_number} {current_question.text} yea(a) or no(b)\n")
+
+        if choice == "a" and current_question.answer == "True" or choice == "b" and current_question.answer == "False":
+            print("perfect")
+        else:
+            print("You are wrong")
 
