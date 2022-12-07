bfe79c5ad7c4c04ada2747becc691cdb951bc274
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-07 10:06:42 +0100

    sketchh basic init

--- etch_a_sketch.py
+++ etch_a_sketch.py
@@ -1,4 +1,13 @@
+from turtle import Turtle, Screen
 
 
 def start():
-    pass
+    my_t = Turtle()
+    my_s = Screen()
+
+    def move_forwards():
+        my_t.forward(10)
+
+    my_s.listen()
+    my_s.onkey(move_forwards, "space")
+    my_s.exitonclick()
