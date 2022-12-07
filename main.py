0683a963d67d2268a09aa61760aa454e728453e4
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-08 00:10:46 +0100

    turtle race logic v1.0

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import spirograph
 import spot_painting
 import event_listener
 import etch_a_sketch
+import turtle_race
 
 # quiz.start()
 # turtle_graphic.start()
 # spirograph.start()
 # spot_painting.start()
 # event_listener.start()
-etch_a_sketch.start()
-
+# etch_a_sketch.start()
+turtle_race.start()
 
---
+++ turtle_race.py
@@ -0,0 +1,31 @@
+import random
+from turtle import Turtle, Screen
+
+
+def start():
+    my_t = Turtle()
+    my_s = Screen()
+    my_s.setup(width=500, height=400)
+    user_bet = my_s.textinput(title="Your bet", prompt="Which turtle will wim the race? Enter a color: ")
+
+    colors = ["red", "blue", "orange", "green", "purple"]
+    all_turtles = []
+    race_active = True
+
+    for turtle_idx in range(5):
+        new_turtle = Turtle(shape="turtle")
+        new_turtle.color(colors[turtle_idx])
+        new_turtle.penup()
+        new_turtle.goto(x=-230, y=-100 + (turtle_idx * 50))
+        all_turtles.append(new_turtle)
+
+    if user_bet:
+        race_active = True
+
+    while race_active:
+        for turtle in all_turtles:
+            rand_distance = random.randint(0, 10)
+            turtle.forward(rand_distance)
+
+
+    my_s.exitonclick()
