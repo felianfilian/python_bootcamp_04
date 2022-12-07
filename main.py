c15fb4ca9f2745f143e7b07440dc6f90fac4b548
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-07 10:01:33 +0100

    etch a sketch init

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import turtle_graphic
 import spirograph
 import spot_painting
 import event_listener
+import etch_a_sketch
 
 # quiz.start()
 # turtle_graphic.start()
 # spirograph.start()
 # spot_painting.start()
-event_listener.start()
+# event_listener.start()
+etch_a_sketch.start()
 
 
