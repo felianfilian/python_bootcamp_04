78ef693d4ce7875ec2f311c74646785947d1d656
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-07 08:42:27 +0100

    colorgram test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import spirograph
 import spot_painting
 
 # quiz.start()
-#turtle_graphic.start()
+# turtle_graphic.start()
 # spirograph.start()
 spot_painting.start()
 
--- spot_painting.py
+++ spot_painting.py
@@ -1,4 +1,14 @@
+import colorgram as cg
 
+def draw_painting(height, width):
+    pass
 
 def start():
-    pass
+    color_list = cg.extract("hirst.jpg", 60)
+    print(color_list)
+    color_palette = []
+
+    for color in color_list:
+        color_palette.append(color.tgb)
+
+    print(color_palette)
