98b0e94ac5421c6e36191fa1c267a1e7c50e4b89
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-07 07:53:38 +0100

    spot painting init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import quiz
 import turtle_graphic
 import spirograph
+import spot_painting
 
 # quiz.start()
 #turtle_graphic.start()
-spirograph.start()
+# spirograph.start()
+spot_painting.start()
 
 
 
