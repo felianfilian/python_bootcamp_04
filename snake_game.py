96b823d9d126b1111d2283e5259f306fc60acb99
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-08 23:16:39 +0100

    automatic segment drawing

--- snake_game.py
+++ snake_game.py
@@ -5,16 +5,13 @@ my_s.setup(width=600, height=600)
 my_s.title("Snake")
 my_s.bgcolor("black")
 
-segment_01 = Turtle("square")
-segment_01.color("white")
+start_pos = [(0, 0), (-20, 0), (-40, 0) ]
 
-segment_02 = Turtle("square")
-segment_02.color("white")
-segment_02.goto(-20, 0)
+for pos in start_pos:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(pos)
 
-segment_03 = Turtle("square")
-segment_03.color("white")
-segment_03.goto(-40, 0)
 
 def snake_up():
     print("test")
