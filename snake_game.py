43167ff10d9b737f6770694596a67b7a380a7cc6
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-09 10:04:35 +0100

    snake class

--- snake_game.py
+++ snake_game.py
@@ -1,51 +1,26 @@
 from turtle import Turtle, Screen
+from snake_snake import Snake
 import time
 
-my_s = Screen()
-my_s.setup(width=600, height=600)
-my_s.title("Snake")
-my_s.bgcolor("black")
-my_s.tracer(0)
-
-start_pos = [(0, 0), (-20, 0), (-40, 0) ]
-segments = []
-
-game_active = True
-
-for pos in start_pos:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(pos)
-    segments.append(new_segment)
-
-while game_active:
-    my_s.update()
-    time.sleep(0.1)
-    for seg in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg -1].xcor()
-        new_y = segments[seg - 1].ycor()
-        segments[seg].goto(new_x, new_y)
-    segments[0].forward(20)
-
-
-
-def snake_up():
-    print("test")
-
-def snake_right():
-    pass
-def snake_down():
-    pass
-def snake_left():
-    pass
-
-
 def start():
+    my_s = Screen()
+    my_s.setup(width=600, height=600)
+    my_s.title("Snake")
+    my_s.bgcolor("black")
+    my_s.tracer(0)
+
+    snake = Snake()
+
     my_s.listen()
-    my_s.onkey(snake_up, "w")
-    my_s.onkey(snake_right, "d")
-    my_s.onkey(snake_down, "s")
-    my_s.onkey(snake_left, "a")
+    my_s.onkey(snake.snake_up, "w")
+    my_s.onkey(snake.snake_right, "d")
+    my_s.onkey(snake.snake_down, "s")
+    my_s.onkey(snake.snake_left, "a")
+
+    game_active = True
+    while game_active:
+        my_s.update()
+        time.sleep(0.1)
+        snake.move()
 
     my_s.exitonclick()
---
+++ snake_snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+START_POS = [(0, 0), (-20, 0), (-40, 0) ]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in START_POS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(pos)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def snake_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def snake_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def snake_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def snake_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
