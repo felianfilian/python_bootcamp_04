23b3a6c525971e330a1fd26a3958e2df4d7bb341
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-09 12:12:04 +0100

    score logic

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,10 @@
+import turtle
 from turtle import Screen
 from snake_snake import Snake
 from snake_food import Food
+from snake_score import Score
 import time
+
 
 def start():
     my_s = Screen()
@@ -12,12 +15,15 @@ def start():
 
     snake = Snake()
     food = Food()
+    score = Score()
 
     my_s.listen()
     my_s.onkey(snake.snake_up, "w")
     my_s.onkey(snake.snake_right, "d")
     my_s.onkey(snake.snake_down, "s")
     my_s.onkey(snake.snake_left, "a")
+
+    turtle.write("Score = 0", True, align="center")
 
     game_active = True
     while game_active:
