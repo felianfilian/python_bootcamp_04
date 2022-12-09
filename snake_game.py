1189ffa95037e143a3bbf3a3131cb1e214b5429b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-09 12:41:09 +0100

    snake extend

--- snake_game.py
+++ snake_game.py
@@ -34,11 +34,14 @@ def start():
         # detect collission with food
         if snake.head.distance(food) < 15:
             food.refresh()
+            snake.extend()
             score.increase_score()
 
         # detect wall
         if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             score.game_over()
             game_active = False
+
+        # detect tail collission
 
     my_s.exitonclick()
--- snake_snake.py
+++ snake_snake.py
@@ -14,11 +14,17 @@ class Snake:
 
     def create_snake(self):
         for pos in START_POS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(pos)
-            self.segments.append(new_segment)
+            self.add_segment(pos)
+
+    def add_segment(self, pos):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg in range(len(self.segments) - 1, 0, -1):
