4b03fe2f37bb1269c1e013ee51f35bb2da96c395
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-11 22:23:18 +0100

    reset score

--- snake_score.py
+++ snake_score.py
@@ -6,6 +6,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -13,11 +14,18 @@ class Score(Turtle):
         self.update_scoreui()
 
     def update_scoreui(self):
+        self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreui()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
